fda337f333d2d3826c5428dd62b2be6542467128
stormhurricane <xstormhurricane@gmail.com>
2026-08-02 20:57:44 +0200

    Removed JQuery & added Fix for 0 height bars

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const array = Array.from({length: 50}, () => Math.floor(Math.random() * 50));
-const max_value = Math.max(...array);
+const arr_obj = create_ancestor_array();
+const array = arr_obj.arr;
+const max_value = arr_obj.max;
 
 let bubble_sort = {
     fn: () => {},
@@ -11,12 +12,28 @@ if(!bubble_sort.finished){
 
 }
 
-let $sort_container = $('<div>', {id: "bubbleSortContainer", class: "algorithm_container"});
-$sort_container.append("<h3>Bubble Sort</h3>");
-let $chart_container = $('<div>', {class:"chart_container"});
+let sort_container = document.createElement('div');
+sort_container.className = 'algorithm_container';
+sort_container.id = "bubbleSortContainer";
+let h_text = document.createElement("h3");
+h_text.textContent = "Bubble Sort";
+sort_container.append(h_text);
+let chart_container = document.createElement('div');
+chart_container.className = "chart_container";
 for(const num of bubble_sort.arr){
-    $chart_container.append(`<div class='bar' style='height:${num / max_value * 100}%'></div>`);
+    let num_container = document.createElement('div');
+    num_container.className = "bar";
+    num_container.style.height = `${num / max_value * 100}%`;
+    chart_container.append(num_container);
 }
-$sort_container.append($chart_container);
+sort_container.append(chart_container);
 
-$('.main_container').append($sort_container);
+document.querySelector('.main_container').append(sort_container);
+
+
+function create_ancestor_array(){
+    const array = Array.from({length: 50}, () => Math.floor(Math.random() * 50) + 1);
+    const max_value = Math.max(...array);
+
+    return {arr: array, max: max_value};
+}
