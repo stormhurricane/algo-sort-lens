fcf27db40d19f459e44fd79144525b097f03d849
stormhurricane <xstormhurricane@gmail.com>
2026-08-04 08:37:44 +0200

    changed array length value

--- array_generator.js
+++ array_generator.js
@@ -3,7 +3,7 @@ export const array = arr_obj.arr;
 export const max_value = arr_obj.max;
 
 function create_ancestor_array(){
-    const array = Array.from({length: 50}, () => Math.floor(Math.random() * 50) + 1);
+    const array = Array.from({length: 30}, () => Math.floor(Math.random() * 30) + 1);
     const max_value = Math.max(...array);
 
     return {arr: array, max: max_value};
